ca156425c19afaba0f0deced8b24f31604b39e03
mchen33 <ming.chen0919@gmail.com>
2016-07-25 13:59:11 -0400

    add biomaterial description onto hoverinfo

--- js/heatmap.js
+++ js/heatmap.js
@@ -10,7 +10,17 @@
         var heatmap_data = [{x, y, z, type}];
         */
         var heatmap_data = eval(settings.tripal_analysis_expression.heatmap_data); 
-        Plotly.newPlot('vis_expression', heatmap_data);
+        var layout = {
+                title: 'Expression Heatmap',
+                xaxis: {
+                  title: 'Treatment'
+                },
+                margin: {
+                  b: 100,
+                  l: 350
+                }     
+            }
+        Plotly.newPlot('vis_expression', heatmap_data, layout);
       })
     }
   }
