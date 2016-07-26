f876e51109e4ffa33c15bd6d71a571c5585af595
mchen33 <ming.chen0919@gmail.com>
2016-07-25 20:50:36 -0400

    adjust left margin

--- js/heatmap.js
+++ js/heatmap.js
@@ -2,21 +2,13 @@
   Drupal.behaviors.tripal_analysis_expression = {
     attach: function(context, settings){
       $(document).ready(function(){
-        /*
-        var x = settings.tripal_analysis_expression.heatmap_x;
-        var y = settings.tripal_analysis_expression.heatmap_y;
-        var z = settings.tripal_analysis_expression.heatmap_z;
-        var type = 'heatmap';
-        var heatmap_data = [{x, y, z, type}];
-        */
         var heatmap_data = eval(settings.tripal_analysis_expression.heatmap_data); 
+        //var layout       = eval(settings.tripal_analysis_expression.heatmap_layout);
         var layout = {
                 title: 'Expression Heatmap',
-                /*
                 xaxis: {
                   title: 'Treatment'
                 },
-                */
                 margin: {
                   b: 100,
                   l: 400
