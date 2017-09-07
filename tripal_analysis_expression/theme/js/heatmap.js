aff4f4f20a9c0261fcf7f3c3cd9389c477e81890
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-09-07 12:41:18 -0400

    Clean up heatmap

--- tripal_analysis_expression/theme/js/heatmap.js
+++ tripal_analysis_expression/theme/js/heatmap.js
@@ -2,11 +2,18 @@
   Drupal.behaviors.tripal_analysis_expression = {
     attach: function (context, settings) {
       $(document).ready(function () {
-        var heatmap_data  = eval(settings.tripal_analysis_expression.heatmap_data)
+        if (typeof settings.tripal_analysis_expression === 'undefined') {
+          return;
+        }
+        else if (typeof settings.tripal_analysis_expression.heatmap_data === 'undefined') {
+          return;
+        }
+
+        var heatmap_data = settings.tripal_analysis_expression.heatmap_data;
         //var layout       =
         // eval(settings.tripal_analysis_expression.heatmap_layout);
-        var left_margin   = settings.tripal_analysis_expression.left_margin
-        var bottom_margin = settings.tripal_analysis_expression.bottom_margin
+        var left_margin   = settings.tripal_analysis_expression.left_margin;
+        var bottom_margin = settings.tripal_analysis_expression.bottom_margin;
         var layout        = {
           title : 'Expression Heatmap',
           /*
@@ -18,9 +25,9 @@
             b: bottom_margin,
             l: left_margin
           }
-        }
-        Plotly.newPlot('vis_expression', heatmap_data, layout)
-      })
+        };
+        Plotly.newPlot('vis_expression', heatmap_data, layout);
+      });
     }
-  }
-})(jQuery)
+  };
+})(jQuery);
