c202fccd71b02a3accad75154c114ecb44f96629
bradfordcondon <bradford.condon@gmail.com>
2017-11-13 09:07:23 -0500

    push for consultation

--- tripal_analysis_expression/theme/js/heatmap.js
+++ tripal_analysis_expression/theme/js/heatmap.js
@@ -1,23 +1,22 @@
+//To make draggable:
+//Listening to plotly_click and then calling the appropriate restyle call should do the trick.
+
 (function ($) {
     Drupal.behaviors.tripal_analysis_expression = {
         attach: function (context, settings) {
             var analyses_options = settings.tripal_analysis_expression.heatmap_data;
 
-            var select = Plotly.d3.select('#select_analysis').append("select").attr("id", "analysis_selector")
+            var select = Plotly.d3.select('#select_analysis')
+                .append("select")
+                .attr("id", "analysis_selector")
+
             Object.keys(analyses_options).map(function (d, i) {
-                console.log(d)
                 select.append("option")
                     .attr("Value", d)
                     .text(d)
             })
-            select.change(function () {//when the selector changes, rebuild the plot
-                    $(function () {
-                        this.setup(settings, heatmap_data);
-                    }.bind(this));
-                }
-            )
             $(function () {
-                this.setup(settings, heatmap_data);
+                this.setup(settings);
             }.bind(this));
         },
 
@@ -34,19 +33,35 @@
             }).node();
 
             //get analysis selected and corresponding heatmap data
-            var selectedAnalysis = Plotly.d3.select('#analysis_selector').find(":selected").text()
+            var selectedAnalysis = $('#analysis_selector').find(":selected").text()
             var heatmap_data = settings.tripal_analysis_expression.heatmap_data[selectedAnalysis]
             var left_margin = settings.tripal_analysis_expression.left_margin;
             var bottom_margin = settings.tripal_analysis_expression.bottom_margin;
             var layout = {
-                title: 'Expression Heatmap',
+                title: selectedAnalysis + "Expression",
                 margin: {
                     l: left_margin,
                     b: bottom_margin
                 }
             };
 
+
             Plotly.plot(node, heatmap_data, layout);
+
+
+            // https://plot.ly/javascript/dropdowns/
+            //alternatively try using a plotly based dropdown instead
+            $("#select_analysis").change(function () {//when the selector changes, rebuild the plot
+                    var node = Plotly.d3.select('#vis_expression').style({
+                        width: '100%'
+                    }).node();
+                    var heatmap_data = settings.tripal_analysis_expression.heatmap_data[selectedAnalysis]
+
+                  //  Plotly.restyle(node, heatmap_data, layout);
+                Plotly.replot(node)
+
+                }
+            ).bind(this)
 
             $(window).on('resize', function () {
                 Plotly.Plots.resize(node);
