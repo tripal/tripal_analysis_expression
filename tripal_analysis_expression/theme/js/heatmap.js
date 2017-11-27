da3808d03b961afd18b2c6a504a1ad97b02cafcd
bradfordcondon <bradford.condon@gmail.com>
2017-11-27 11:46:04 -0500

    fix bug in heatmap generating code and remove dummy data input

--- tripal_analysis_expression/theme/js/heatmap.js
+++ tripal_analysis_expression/theme/js/heatmap.js
@@ -8,27 +8,28 @@
 
             //========= only data from one analysis is available. Add some fake data to show the change when drop down value changes =====
             var hd = settings.tripal_analysis_expression.heatmap_data;
-            var analyses_options = {
-                'That one time we created biomaterials and expression': hd['That one time we created biomaterials and expression'],
-                'second analysis': [{
-                    hoverinfo: 'all',
-                    text: [
-                        ['a', 'b', 'c'],
-                        ['a', 'b', 'c'],
-                        ['a', 'b', 'c'],
-                        ['a', 'b', 'c'],
-                    ],
-                    x: ['label 1', 'label 2', 'label 3'],
-                    y: ['feature 1', 'feature 2', 'feature 3', 'feature 4'],
-                    z: [
-                        [1, 2, 4],
-                        [3, 1, 20],
-                        [3, 1, 20],
-                        [3, 10, 20],
-                    ]
-                }]
-            };
-            //=======================================
+            // var analyses_options = {
+            //     'That one time we created biomaterials and expression': hd['That one time we created biomaterials and expression'],
+            //     'second analysis': [{
+            //         hoverinfo: 'all',
+            //         text: [
+            //             ['a', 'b', 'c'],
+            //             ['a', 'b', 'c'],
+            //             ['a', 'b', 'c'],
+            //             ['a', 'b', 'c'],
+            //         ],
+            //         x: ['label 1', 'label 2', 'label 3'],
+            //         y: ['feature 1', 'feature 2', 'feature 3', 'feature 4'],
+            //         z: [
+            //             [1, 2, 4],
+            //             [3, 1, 20],
+            //             [3, 1, 20],
+            //             [3, 10, 20],
+            //         ]
+            //     }]
+            // };
+            // //=======================================
+            var analyses_options = hd;
 
 
             // build drop down UI
@@ -65,10 +66,9 @@
             make_heatmap(Object.keys(analyses_options)[0]);
 
             function make_heatmap(selectedAnalysis) {
-                console.log(analyses_options);
-                console.log(selectedAnalysis + ' some analysis');
+                console.log(analyses_options)
                 var heatmap_data = [analyses_options[selectedAnalysis][0]];
-                // var heatmap_data = [settings.tripal_analysis_expression.heatmap_data[selectedAnalysis]];
+                console.log(heatmap_data)
                 var left_margin = settings.tripal_analysis_expression.left_margin;
                 var bottom_margin = settings.tripal_analysis_expression.bottom_margin;
                 var layout = {
