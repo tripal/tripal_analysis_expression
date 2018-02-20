260d3d122bc302f0279079da619b20e9e28be992
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-02-20 14:36:15 -0500

    Lets test the speed before adding the rest of properties

--- tripal_analysis_expression/theme/js/heatmap.js
+++ tripal_analysis_expression/theme/js/heatmap.js
@@ -1,104 +1,154 @@
-//To make draggable:
-//Listening to plotly_click and then calling the appropriate restyle call should do the trick.
-
 (function ($) {
-    Drupal.behaviors.tripal_analysis_expression = {
-        attach: function (context, settings) {
-            // var analyses_options = settings.tripal_analysis_expression.heatmap_data;
+  // To make draggable:
+  // Listening to plotly_click and then calling the appropriate restyle call
+  // should do the trick.
+  Drupal.behaviors.tripal_analysis_expression = {
+    attach: function (context, settings) {
+      this.settings = settings.tripal_analysis_expression;
+      this.cache = {};
+      if (!this.settings) {
+        return;
+      }
 
-            //========= only data from one analysis is available. Add some fake data to show the change when drop down value changes =====
-            var hd = settings.tripal_analysis_expression.heatmap_data;
-            var features = settings.tripal_analysis_expression.feature_list;
-            console.log(features);
-            var analysis_legend = settings.tripal_analysis_expression.analysis_legend;
+      this.data = this.settings.data;
 
-            var analyses_options = hd;
+      // Build drop down UI
+      var $selectContainer = $('#select_analysis');
+      this.$select = $('<select />');
+      $selectContainer
+          .append(this.$select)
+          .attr('id', 'analysis_selector');
 
+      Object.keys(this.data.analyses).map(function (key) {
+        var $option = $('<option />', {
+          value: key
+        }).text(this.data.analyses[key]);
+        this.$select.append($option);
+      }.bind(this));
 
-            console.log(analyses_options);
+      this.selectedAnalysis = parseInt(this.$select.val());
 
-            // build drop down UI
-            var select = Plotly.d3.select('#select_analysis')
-                .append('select')
-                .attr('id', 'analysis_selector');
+      // Set the download link
+      this.updateDownloadLink();
 
-            Object.keys(analyses_options).map(function (d) {
-                select.append('option')
-                    .attr('Value', d)
-                    .text(d);
-            });
+      this.setup();
+    },
 
-            //get analysis and build link for download
-            var selectedAnalysis = document.querySelector('#analysis_selector').value;
-            var link = '/tripal/analysis-expression/download?feature_ids=' + features[selectedAnalysis] + '&analysis_id=' + analysis_legend[selectedAnalysis];
-            Plotly.d3.select('#heatmap_download')
-                .attr('href', link);
+    updateDownloadLink: function () {
+      // Get analysis and build link for download
+      var feature_ids = Object.keys(this.data.data[this.selectedAnalysis]).map(function (key) {
+        return key;
+      }).join(',');
 
-            $(function () {
-                this.setup(settings, analyses_options);
-            }.bind(this));
-        },
+      var link = this.settings.baseURL + '/tripal/analysis-expression/download?feature_ids=' + feature_ids + '&analysis_id=' + this.selectedAnalysis;
+      $('#heatmap_download').attr('href', link);
+    },
 
-        setup: function (settings, analyses_options) {
-            if (typeof settings.tripal_analysis_expression === 'undefined') {
-                return;
-            }
-            else if (typeof settings.tripal_analysis_expression.heatmap_data === 'undefined') {
-                return;
-            }
+    constructHeatMapData: function (expression) {
+      if (typeof this.cache[this.selectedAnalysis] !== 'undefined') {
+        return this.cache[this.selectedAnalysis];
+      }
 
-            var node = Plotly.d3.select('#vis_expression').style({
-                width: '100%'
-            }).node();
+      console.log(expression[this.selectedAnalysis], this.selectedAnalysis);
 
-            // by default, the fist analysis from the dropdown is used to plot.
-            make_heatmap(Object.keys(analyses_options)[0]);
+      // Extract data, features and biomaterials for the current analysis
+      var data = {
+        biomaterials: expression.biomaterials[this.selectedAnalysis],
+        features: expression.features[this.selectedAnalysis],
+        matrix: expression.data[this.selectedAnalysis]
+      };
 
-            function make_heatmap(selectedAnalysis) {
-                var heatmap_data = [analyses_options[selectedAnalysis][0]];
-                var left_margin = settings.tripal_analysis_expression.left_margin;
-                var bottom_margin = settings.tripal_analysis_expression.bottom_margin;
-                var layout = {
-                    title: selectedAnalysis + ' Expression',
-                    margin: {
-                        l: left_margin,
-                        b: bottom_margin
-                    }
-                };
+      var features = Object.keys(data.features).map(function (key) {
+        return data.features[key];
+      }.bind(this));
 
-                Plotly.newPlot('vis_expression', heatmap_data, layout);
+      var biomaterials = Object.keys(data.biomaterials).map(function (key) {
+        return data.biomaterials[key].name;
+      }.bind(this));
 
-                var analysisSelector = document.querySelector('#analysis_selector');
+      var values = [];
 
-                function update_heatmap() {
-                    make_heatmap(analysisSelector.value);
-                }
+      Object.keys(data.features).map(function (feature_id) {
+        var row = [];
+        Object.keys(data.biomaterials).map(function (biomaterial_id) {
+          if (typeof data.matrix[feature_id][biomaterial_id] !== 'undefined') {
+            row.push(parseFloat(data.matrix[feature_id][biomaterial_id]));
+          }
+          else {
+            row.push(0);
+          }
+        });
 
-                analysisSelector.addEventListener('change', update_heatmap, false);
-            }
+        values.push(row);
+      });
 
+      this.cache[this.selectedAnalysis] = {
+        x: biomaterials,
+        y: features,
+        z: values
+      };
 
-            var download_link = $('#heatmap_download');
-            var download_message = $('<span />');
-            download_link.after(download_message);
-            download_link.click(function (e) {
-                e.preventDefault();
-                var src = $(this).attr('href');
-                download_message.html(' Creating file. Download will start automatically ...');
-                var iframe = $('<iframe />', {
-                    src: src,
-                    width: 1,
-                    height: 1
-                });
-                $('body').append(iframe);
-            });
+      return this.cache[this.selectedAnalysis];
+    },
 
-            $(window).on('resize', function () {
-                Plotly.Plots.resize(node);
-            });
+    /**
+     * Set the scene and create the heat map
+     */
+    setup: function () {
+      var _that = this;
+      this.$select.on('change', function () {
+        _that.selectedAnalysis = parseInt($(this).val());
+        console.log($(this).val(), 'value');
+        _that.draw(_that.constructHeatMapData(_that.data));
+        _that.updateDownloadLink.call(_that);
+      });
+
+      var node = Plotly.d3.select('#expression_heat_map_canvas').style({
+        width: '100%'
+      }).node();
+
+      var download_link = $('#heatmap_download');
+      var download_message = $('<span />');
+      download_link.after(download_message);
+      download_link.click(function (e) {
+        e.preventDefault();
+        var src = $(this).attr('href');
+        download_message.html(' Creating file. Download will start automatically ...');
+        var iframe = $('<iframe />', {
+          src: src,
+          width: 1,
+          height: 1
+        });
+        $('body').append(iframe);
+      });
+
+      $(window).on('resize', function () {
+        Plotly.Plots.resize(node);
+      });
+
+      this.draw(this.constructHeatMapData(this.data));
+    },
+
+    /**
+     * Create the heat map
+     */
+    draw: function (data) {
+      var left_margin = 12; //this.settings.left_margin;
+      var bottom_margin = 13; //this.settings.bottom_margin;
+      var layout = {
+        title: this.data.analyses[this.selectedAnalysis] + ' Expression',
+        margin: {
+          l: left_margin,
+          b: bottom_margin
         }
+      };
 
-        //Attach the download link
+      var chartData = data;
+      chartData.type = 'heatmap';
 
-    };
+      console.log(chartData);
+
+      Plotly.newPlot('expression_heat_map_canvas', [chartData]);
+    }
+  };
 })(jQuery);
