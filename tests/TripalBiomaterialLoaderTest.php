bc2f0de2f85d530d5182d44cdcb93c06d6ac81d7
bradford.condon <bradford.condon@gmail.com>
2018-09-20 12:48:25 -0400

    refactor xml sampler

--- tests/TripalBiomaterialLoaderTest.php
+++ tests/TripalBiomaterialLoaderTest.php
@@ -9,41 +9,6 @@ class TripalBiomaterialLoaderTest extends TripalTestCase {
 
 
 
-
-   public function test_biomat_loader_check_functionality(){
-     module_load_include('inc', 'tripal_biomaterial', 'includes/TripalImporter/tripal_biomaterial_loader_v3');
-
-     $importer = new \tripal_biomaterial_loader_v3();
-
-     $organism = factory('chado.organism')->create();
-     $analysis = factory('chado.analysis')->create();
-
-     //before loading, insert some cvterms that will match the properties and values
-
-     $cv_name = 'TAE_TEST_SUITE';
-     $cv = factory('chado.cv')->create(['name' => $cv_name]);
-
-     $prop_term = factory('chado.cvterm')->create(['name' => 'city', 'cv_id' => $cv->cv_id]);
-     $value_term = factory('chado.cvterm')->create(['name' => 'blood', 'cv_id' => $cv->cv_id]);
-
-     $file_path = __DIR__ .'/../example_files/biomaterials_example.xml';
-     $output = tripal_biomaterial_test_biosample_cvterms_xml($file_path, $organism->organism_id, $analysis->analysis_id);
-
-     $this->assertNotFalse($output);
-     $this->assertArrayHasKey('attributes', $output);
-     $this->assertArrayHasKey('values', $output);
-
-     //TODO: The form manually deals with this stuff, and it does it 2x, once for properties and once for values.  it should be refactored.
-
-     $fields = $output["attributes"];
-     $this->assertArrayHasKey('camera', $fields);
-     $this->assertArrayHasKey('city', $fields);
-
-     $property_values = $output["values"];
-   }
-
-
-
   /**
    * @group biomaterial
    * @group importer
@@ -79,4 +44,19 @@ class TripalBiomaterialLoaderTest extends TripalTestCase {
      $this->assertEquals(5, count($results));
    }
 
+  /**
+   * @group wip
+   */
+   public function test_parse_xml_biomaterial_file(){
+     $file =  __DIR__ .'/../example_files/biomaterials_example.xml';
+
+     module_load_include('inc', 'tripal_biomaterial', 'includes/TripalImporter/tripal_biomaterial_loader_v3');
+
+     $importer = reflect(new \tripal_biomaterial_loader_v3());
+
+    $out =  $importer->parse_xml_biomaterial_file($file);
+     $fields = $out["attributes"];
+     $this->assertArrayHasKey('camera', $fields);
+     $this->assertArrayHasKey('city', $fields);
+   }
 }
