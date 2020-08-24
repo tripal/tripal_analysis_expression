ffc033d5f9193f83a9f6f45ac69fd33b1a06489b
Abdullah Almsaeed <almasaeed2010@gmail.com>
2020-08-24 15:00:18 -0400

    Fix tooltip position

--- tripal_analysis_expression/theme/js/expression.js
+++ tripal_analysis_expression/theme/js/expression.js
@@ -540,17 +540,17 @@
         var bar = d3.event.target
         $(bar).css('opacity', .8)
         var propTable = _that.buildPropertyTooltipTable(d)
-        divTooltip.transition()
-          .duration(200)
-          .style('opacity', 1)
-          .style('display', 'block')
+        // divTooltip.transition()
+        //   .duration(200)
+        //   .style('opacity', 1)
+        //   .style('display', 'block')
 
         divTooltip.html(
           '<strong>Biosample:</strong> <a href="/bio_data/' + d.node + '">' + d.name + '</a><br/>' +
           '<strong>Expression: </strong>' + d.intensity + ' ' + d.units + '<br/>' +
           '<strong>Description: </strong><br/>' + d.description + '<br/>'
           + propTable)
-          .style('left', ($(this).offset().left - 200) + 'px')
+          .style('left', ($(this).offset().left - 260) + 'px')
           .style('top', ((d3.event.pageY - (200)) + 'px'))
       })
         .on('mouseout', function () {
